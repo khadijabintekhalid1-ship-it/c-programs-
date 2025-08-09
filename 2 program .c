b2e32f29efebab4776bb4bd11108655bb400e94e
khadijabintekhalid1-ship-it <khadijabintekhalid1@gmail.com>
2025-08-09 14:40:31 +0500

    Add files via upload

---
+++ 2 program .c
@@ -0,0 +1,8 @@
+#include <stdio.h>  // Standard input-output library
+
+int main() {
+    printf("CYBER SECURITY CLASS \n");
+	printf(" WELCOME to Learn C \t lets learn ! \n"); 
+    return 0; // End of program
+}
+
