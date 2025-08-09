000f4cd38cb5c3f8dbf340c10d11fc4174d74b04
khadijabintekhalid1-ship-it <khadijabintekhalid1@gmail.com>
2025-08-09 14:46:17 +0500

    C Program – Display Name, Age, and Height
    C Program – Display Name, Age, and Height
    This is a simple C program that demonstrates the use of different data types:
    
    char for storing text (name)
    
    int for storing whole numbers (age)
    
    float for storing decimal values (height)
    
    The program prints the stored values to the console using printf with format specifiers (%s, %d, %f).

---
+++ Untitled3.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () 
+{
+	int age = 9;
+	float height  = 7.7;
+	char name[50]="khan";
+	printf (" my age is %d my height is %2f my name is %s",age,height,name);
+	return 0;
+}
