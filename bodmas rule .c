30812a88cd3192ea1a99fc42de67e89a7cec4867
khadijabintekhalid1-ship-it <khadijabintekhalid1@gmail.com>
2025-08-12 14:25:50 +0500

    bodmas
    Step-by-step BODMAS:
    
    Bracket: (a + b) → 2 + 3 = 5
    
    Multiplication: 5 * c → 5 * 4 = 20
    
    Division: d / b → 6 / 3 = 2
    
    Subtraction: 20 - 2 = 18
    
    Output:
    Result: 18

---
+++ bodmas rule .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2, b = 3, c = 4, d = 6;
+    
+    // Expression: (a + b) * c - d / b
+    int result = (a + b) * c - d / b;
+
+    printf("Result: %d", result);
+
+    return 0;
+}
+
