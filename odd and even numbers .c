ace4b183dd52785aed7e96eca96f3694c66a7cc9
khadijabintekhalid1-ship-it <khadijabintekhalid1@gmail.com>
2025-08-16 11:46:03 +0500

    Add files via upload
    This program is written in C language.
    It checks whether a given integer is Even or Odd.

---
+++ odd and even numbers .c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main ()
+{
+	int a;
+	printf ("enter your number ");
+	scanf ("%d",& a);
+	if (a % 2 ==0){
+		printf ("This is an EVEN NUMBER ");
+	}else {
+		printf ("This is an ODD NUMBER ");
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
